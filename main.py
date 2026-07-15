7f08861967f0b60bcd1e7fa6f1978608217292b4
Jackson Huff <engineer@ucf.edu>
2026-07-15 16:24:29 -0400

    Improve

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
 import dearpygui.dearpygui as dpg
 import playsound3
+from math import sin, cos
+
+
+
+sindatax = []
+sindatay = []
+for i in range(0, 500):
+    sindatax.append(i / 1000)
+    sindatay.append(0.5 + 0.5 * sin(50 * i / 1000))
+
+def show_error_window(message):
+    with dpg.window(label="Internal Error", modal=True, tag="error_window"):
+        dpg.add_text(f"An error occurred: {message}")
+        dpg.add_button(label="Close", callback=lambda: dpg.delete_item("error_window"))
+        playsound3.playsound("src/half-life-2-episode-2-base-alarm.mp3", block=False)
+
+    while dpg.does_item_exist("error_window"):
+        dpg.render_dearpygui_frame()
 
 class ifmstate:
     gathering = False
     has_gage = False
 
 ifm = ifmstate()
+
+try: 
+    import PyGage
+    import GageSupport as gs
+    import GageConstants as gc
+    ifm.has_gage = True
+except ImportError:
+    print("Running in Gage-less mode. PyGage module not found.")
 
 def button1_callback(sender, app_data):
     print("Button 1 clicked")
@@ -25,13 +51,7 @@ def button1_callback(sender, app_data):
 
 def main():
 
-    try: 
-        import PyGage
-        import GageSupport as gs
-        import GageConstants as gc
-        ifm.has_gage = True
-    except ImportError:
-        print("Running in Gage-less mode. PyGage module not found.")
+
 
 
     # Setup Dear PyGUI
@@ -39,16 +59,31 @@ def main():
     dpg.create_viewport(title="Interferomatic", width=800, height=600)
 
     with dpg.font_registry():
-        default_font = dpg.add_font("src/ScienceGothic-Medium.ttf", 40)
-        giant_font = dpg.add_font("src/ScienceGothic-Medium.ttf", 80)
+        default_font = dpg.add_font("src/ScienceGothic-Medium.ttf", 80)
+        giant_font = dpg.add_font("src/ScienceGothic-Medium.ttf", 160)
+        small_font = dpg.add_font("src/ScienceGothic-Medium.ttf", 50)
     
     dpg.bind_font(default_font)
 
     with dpg.window(label="Main Window", tag="main_window"):
-        dpg.add_text("Hello, world")
         dpg.add_button(label="Start", tag="startstop_button", callback=button1_callback)
-        dpg.add_input_text(label="string", default_value="Quick brown fox")
-        dpg.add_slider_float(label="float", default_value=0.273, max_value=1)
+        
+        dpg.add_input_int(label="Interferograms to collect", tag="interferograms_input", default_value=100000, width=600)
+        with dpg.tooltip("interferograms_input"):
+            dpg.add_text("Number of interferograms to collect before stopping")
+
+        dpg.add_slider_float(label="Cross correlational threshold", tag="threshold_slider", default_value=0.5, max_value=1, width=600)
+        with dpg.tooltip("threshold_slider"):
+            dpg.add_text("Minimum threshold for cross-correlation to trigger interferogram averaging")
+
+        dpg.add_button(label="Fullscreen", tag="fullscreen_button", callback=lambda: dpg.toggle_viewport_fullscreen())
+        dpg.add_button(label="Exit", callback=lambda: dpg.stop_dearpygui())
+
+        with dpg.plot(label="Live View", tag="chart1", height=800, width=1600):
+            dpg.add_plot_legend()
+            dpg.add_plot_axis(dpg.mvXAxis, label="samples")
+            dpg.add_plot_axis(dpg.mvYAxis, label="amplitude", tag="amp")
+            dpg.add_line_series(sindatax, sindatay, label="Channel 1", parent="amp", tag="series1")
 
         dpg.bind_item_font("startstop_button", giant_font)
 
@@ -66,8 +101,11 @@ def main():
 
 
     dpg.bind_item_theme("startstop_button", "start_button_theme")
+    dpg.bind_item_font("chart1", small_font)
 
     dpg.setup_dearpygui()
+
+    dpg.maximize_viewport()
 
     dpg.set_primary_window("main_window", True)
 
@@ -77,11 +115,24 @@ def main():
     dpg.start_dearpygui()
 
 
+    
+
+    if not ifm.has_gage:
+        return
+    
+    try:
+        run_gage()
+    except Exception as e:
+        # Show an error window
+        show_error_window(str(e))
+
     dpg.destroy_context()
 
-    if not has_gage:
-        return
 
+def run_gage():
+    import PyGage
+    import GageSupport as gs
+    import GageConstants as gc
     # 1. Init + open first system
     status = PyGage.Initialize()
     handle = PyGage.GetSystem(0, 0, 0, 0)
